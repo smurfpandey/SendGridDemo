b86fc60a54f6777217589b46088eefa32f0de917
Neeraj <neeraj.viman@gmail.com>
2014-06-16 22:18:02 +0530

    Sendgrid demo on net4.0 project

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Create the email object first, then add the properties.
+            SendGridMessage myMessage = new SendGridMessage();
+            myMessage.AddTo("anna@example.com");
+            myMessage.From = new MailAddress("john@example.com", "John Smith");
+            myMessage.Subject = "Testing the SendGrid Library";
+            myMessage.Text = "Hello World!";
+
+            // Create credentials, specifying your user name and password.
+            var credentials = new NetworkCredential("username", "password");
+
+            // Create an Web transport for sending email.
+            var transportWeb = new Web(credentials);
+
+            // Send the email.
+            transportWeb.Deliver(myMessage);
+        }
+    }
+}
